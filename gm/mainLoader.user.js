cc0a329caeb79243ae4e0aff96c377a9458b61de
angal antain <mail@angal.ru>
2015-05-29 01:57:36 +0300

    Fixed FireFox issue

--- gm/mainLoader.user.js
+++ gm/mainLoader.user.js
@@ -3,44 +3,49 @@
 // @namespace    ontando.io.agar
 // @updateURL    https://rawgit.com/antain/blobster/master/gm/mainLoader.user.js
 // @downloadURL  https://rawgit.com/antain/blobster/master/gm/mainLoader.user.js
-// @version      0.1.1
+// @version      0.1.2
 // @description  Arag.IO script group loader
 // @author       ontando (angal)
-// @match        agar.io
+// @include     http://agar.io/
+// @include     http://agar.io/*
+// @include     http://*.agar.io/*
+// @include     https://agar.io/
+// @include     https://agar.io/*
+// @include     https://*.agar.io/*
 // @grant        GM_setClipboard
 // @grant        GM_setValue
 // @grant        GM_getValue
 // ==/UserScript==
 
 (function(w, $) {
-	var localhost = "localhost:8000";
-	w.ontando_mainLoader_localhost = localhost;
-	w.ontando_mainLoader_load = function() {
-		w.ontando.gm_handler.init(GM_setClipboard, GM_getValue, GM_setValue);
-	};
+    var localhost = "localhost:8000";
+    w.ontando_mainLoader_localhost = localhost;
+    w.ontando_mainLoader_load = function() {
+        w.ontando.gm_handler.init(GM_setClipboard, GM_getValue, GM_setValue);
+    };
     
-	var pushScript = function(src, override) {
-		var script = document.createElement('script');
-		script.type = "text/javascript";
-		script.override = override;
-		script.src = src;
-		document.head.appendChild(script);
-	};
+    var pushScript = function(src, override) {
+        var script = document.createElement('script');
+        script.type = "text/javascript";
+        script.override = override;
+        script.src = src;
+        document.head.appendChild(script);
+    };
     
-	var github = function (name) { 
-		pushScript("https://rawgit.com/antain/blobster/master/" + name + "?_=" + new Date().getTime(), 0);
-	};
-	var local = function (name) {
-		pushScript("http://" + localhost + "/" + name + "?_=" + new Date().getTime(), 1);
-	};
+    var github = function (name) { 
+        pushScript("https://rawgit.com/antain/blobster/master/" + name + "?_=" + new Date().getTime(), 0);
+    };
+    var local = function (name) {
+        pushScript("http://" + localhost + "/" + name + "?_=" + new Date().getTime(), 1);
+    };
     
-	github("core/base.js");
-	github("core/modAPI.js");
-	github("mods/githubLoader.js");
+    github("core/base.js");
+    github("core/modAPI.js");
+    github("mods/githubLoader.js");
 
-	local("blobster/core/base.js");
-	local("blobster/core/modAPI.js");
-	local("blobster/mods/modsLoader.js");
-	local("localLoader.js");
+    local("blobster/core/base.js");
+    local("blobster/core/modAPI.js");
+    local("blobster/mods/modsLoader.js");
+    local("localLoader.js");
     
 }) (unsafeWindow, jQuery);
